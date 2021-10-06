34b1ea9f4069d0cc964aa9d06380831b4ec9a6e0
tataPongpol <pongpol.ro@ku.th>
2021-10-07 05:15:04 +0700

    add data to UserCouponSeeder

--- database/seeders/UserCouponSeeder.php
+++ database/seeders/UserCouponSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\UserCoupon;
 use Illuminate\Database\Seeder;
 
 class UserCouponSeeder extends Seeder
@@ -13,6 +14,49 @@ class UserCouponSeeder extends Seeder
      */
     public function run()
     {
-        //
+        // user id 1
+        // coupon id 1
+        $user_coupon = new UserCoupon();
+        $user_coupon->user_id = 1;
+        $user_coupon->coupon_id = 1;
+        $user_coupon->coupon_status= "used";
+        $user_coupon->reviewed = true;
+        $user_coupon->save();
+
+        // user id 1
+        // coupon id 2
+        $user_coupon = new UserCoupon();
+        $user_coupon->user_id = 1;
+        $user_coupon->coupon_id = 2;
+        $user_coupon->coupon_status= "used";
+        $user_coupon->reviewed = false;
+        $user_coupon->save();
+
+        // user id 1
+        // coupon id 3
+        $user_coupon = new UserCoupon();
+        $user_coupon->user_id = 1;
+        $user_coupon->coupon_id = 3;
+        $user_coupon->coupon_status= "unuse";
+        $user_coupon->reviewed = true;
+        $user_coupon->save();
+
+        // user id 2
+        // coupon id 4
+        $user_coupon = new UserCoupon();
+        $user_coupon->user_id = 2;
+        $user_coupon->coupon_id = 4;
+        $user_coupon->coupon_status= "unuse";
+        $user_coupon->reviewed = false;
+        $user_coupon->save();
+
+        // user id 2
+        // coupon id 5
+        $user_coupon = new UserCoupon();
+        $user_coupon->user_id = 2;
+        $user_coupon->coupon_id = 5;
+        $user_coupon->coupon_status= "used";
+        $user_coupon->reviewed = false;
+        $user_coupon->save();
     }
 }
