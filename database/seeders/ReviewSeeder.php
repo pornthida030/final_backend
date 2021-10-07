7d8b9d6c41fec8c082598ae51f0785b40c91fe11
6210451314 <pornthida.suks@ku.th>
2021-10-07 13:56:41 +0700

    add Seeder about Review

--- database/seeders/ReviewSeeder.php
+++ database/seeders/ReviewSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Review;
 
 class ReviewSeeder extends Seeder
 {
@@ -13,6 +14,32 @@ class ReviewSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $review = new Review();
+        $review->user_id = 2;
+        $review->coupon_id = 1;
+        $review->review_detail = "ช่างทำดีมากค่ะ";
+        $review->score = 4;
+        $review->save();    
+
+        $review = new Review();
+        $review->user_id = 2;
+        $review->coupon_id = 6;
+        $review->review_detail = "พนักงานบริการดีค่ะ แต่รอนานไปหน่อย";
+        $review->score = 3;
+        $review->save();  
+
+        $review = new Review();
+        $review->user_id = 2;
+        $review->coupon_id = 3;
+        $review->review_detail = "สีผมโดนใจมากค่ะ บริการดีมากเลยค่ะ";
+        $review->score = 5;
+        $review->save();  
+
+        $review = new Review();
+        $review->user_id = 2;
+        $review->coupon_id = 5;
+        $review->review_detail = "นวดสบายมากค่ะ";
+        $review->score = 4;
+        $review->save();  
     }
 }
