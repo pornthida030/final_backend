6837c172bb9c411cc33aad737c6303f7a8dd8437
nichanow <nichathitra.m@ku.th>
2021-10-11 20:56:21 +0700

    create DiscountCoupon

---
+++ app/Http/Controllers/DiscountCouponController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\DiscountCoupon;
+
+class DiscountCouponController extends Controller
+{
+    // ทำ crate, update, destory 
+}
---
+++ database/migrations/2021_10_11_115156_create_discount_coupons_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDiscountCouponsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('discount_coupons', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('discount_coupons');
+    }
+}
