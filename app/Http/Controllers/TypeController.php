f384f658184e5f7bc604b838950f8c2405bbd1c2
thefirstblair <thanyatorn.kr@ku.th>
2021-10-16 05:34:28 +0700

    make image uploader for Type

--- app/Http/Controllers/TypeController.php
+++ app/Http/Controllers/TypeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
 use App\Models\Type;
+use Illuminate\Support\Facades\Storage;
 
 class TypeController extends Controller
 {
@@ -16,6 +17,9 @@ class TypeController extends Controller
         foreach ($type as $papa) {
             $papa['service_count'] = $papa->services()->count();
             $papa['coupon_count'] = $papa->coupons()->count();
+
+            $papa["type_image_url"] = env('APP_URL', false).Storage::url($papa["type_image_url"]);
+
             $papa['employees'] = $papa->employees($papa['id'])->get();
             foreach ($papa['employees'] as $employ) {
                 $employ['name'] = $employ->user()->value('name');
@@ -48,8 +52,7 @@ class TypeController extends Controller
         // validator
         $validator = Validator::make($request->all(), [
             'name' => 'required',
-            'type_image_url' => 'required',
-
+            'type_image_url' => 'required|image',
         ]);
 
         if ($validator->fails()) {
@@ -62,9 +65,15 @@ class TypeController extends Controller
         } else {
             $type = new Type();
             $type->name = $request->name;
-            $type->type_image_url = $request->type_image_url;
+
+            // บันทึก path รูปภาพลง database type
+            $type->type_image_url = 'image_upload/'.$request->type_image_url->hashName();
+
+            // บันทึกรูปภาพลง storage/image_upload
+            $request->type_image_url->store('image_upload','public');
 
             if ($type->save()) {
+                $type["type_image_url"] = env('APP_URL', false).Storage::url('image_upload/'.$request->type_image_url->hashName());
                 return $type;
             } else {
                 return
@@ -82,7 +91,7 @@ class TypeController extends Controller
         $type = Type::findOrFail($id);
         $validator = Validator::make($request->all(), [
             'name' => 'required',
-            'type_image_url' => 'required',
+            'type_image_url' => 'image',
         ]);
 
         if ($validator->fails()) {
@@ -94,11 +103,22 @@ class TypeController extends Controller
             ];
         } else {
             $type->name = $request->name;
-            $type->type_image_url = $request->type_image_url;
+
+            if(!empty($request->type_image_url)){
+                $type->type_image_url = 'image_upload/'.$request->type_image_url->hashName();
+                $request->type_image_url->store('image_upload','public');
+            }
 
             if ($type->save()) {
                 $type['service_count'] = $type->services()->count();
                 $type['coupon_count'] = $type->coupons()->count();
+
+                if(!empty($request->type_image_url)){
+                    $type["type_image_url"] = env('APP_URL', false).Storage::url('image_upload/'.$request->type_image_url->hashName());
+                }else{
+                    $type["type_image_url"] = env('APP_URL', false).Storage::url($type["type_image_url"]);
+                }
+
                 return $type;
             } else {
                 return
@@ -118,6 +138,7 @@ class TypeController extends Controller
         $type->coupons()->delete();
 
         if ($type->delete()) {
+            Storage::disk('public')->delete($type['type_image_url']);
             return [
                 "status" => "success"
             ];
