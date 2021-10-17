efbb058c530c042fada6256bb87a05fae1e41ff0
nichanow <nichathitra.m@ku.th>
2021-10-17 07:03:29 +0700

    add data

--- database/seeders/CouponSeeder.php
+++ database/seeders/CouponSeeder.php
@@ -14,37 +14,37 @@ class CouponSeeder extends Seeder
      */
     public function run()
     {
-        // ย้อมผม service id 1
+        // type id 1 : ซาลอน
+        // service id 1 : สระผม
         // coupon id 1
         $coupon = new Coupon();
         $coupon->type_id = 1;
         $coupon->service_id = 1;
-        $coupon->name = "ย้อมผม+กัดสีผม 1 รอบ";
-        $coupon->price = 500;
-        $coupon->time = 100; 
+        $coupon->name = "สระผมธรรมดา";
+        $coupon->price = 200;
+        $coupon->time = 30; 
         $coupon->save();
 
-        // ย้อมผม service id 1
         // coupon id 2
         $coupon = new Coupon();
         $coupon->type_id = 1;
         $coupon->service_id = 1;
-        $coupon->name = "ย้อมผม+กัดสีผม ด้วยผลิตภัณฑ์ออแกนิก";
-        $coupon->price = 2000;
-        $coupon->time = 190; 
+        $coupon->name = "สระผม + ทำทรีตเมนต์";
+        $coupon->price = 500;
+        $coupon->time = 90; 
         $coupon->save();
 
-        // ย้อมผม service id 1
+        // type id 1 : ซาลอน
+        // service id 2 : ตัดผม
         // coupon id 3
         $coupon = new Coupon();
         $coupon->type_id = 1;
-        $coupon->service_id = 1;
-        $coupon->name = "ย้อมผม+ทำไฮไลท์";
-        $coupon->price = 800;
-        $coupon->time = 190; 
+        $coupon->service_id = 2;
+        $coupon->name = "ตัดผมแบบยูนาบี";
+        $coupon->price = 1000;
+        $coupon->time = 60; 
         $coupon->save();
 
-        // ตัดผม service id 2
         // coupon id 4
         $coupon = new Coupon();
         $coupon->type_id = 1;
@@ -54,49 +54,219 @@ class CouponSeeder extends Seeder
         $coupon->time = 30; 
         $coupon->save();
 
-        // นวดฝ่าเท้า service id 3
+        // type id 1 : ซาลอน
+        // service id 3 : ย้อมสีผม
         // coupon id 5
         $coupon = new Coupon();
-        $coupon->type_id = 2;
+        $coupon->type_id = 1;
         $coupon->service_id = 3;
-        $coupon->name = "นวดฝ่าเท้าด้วยน้ำมันมวย";
-        $coupon->price = 100;
-        $coupon->time = 15; 
+        $coupon->name = "ย้อมผม+กัดสีผม";
+        $coupon->price = 2000;
+        $coupon->time = 150; 
         $coupon->save();
 
-        // นวดหน้า service id 4
         // coupon id 6
         $coupon = new Coupon();
-        $coupon->type_id = 2;
+        $coupon->type_id = 1;
+        $coupon->service_id = 3;
+        $coupon->name = "ย้อมผม+ทำไฮไลท์";
+        $coupon->price = 800;
+        $coupon->time = 150; 
+        $coupon->save();
+
+        // type id 1 : ซาลอน
+        // service id 4 : ดัดผม
+        // coupon id 7
+        $coupon = new Coupon();
+        $coupon->type_id = 1;
         $coupon->service_id = 4;
-        $coupon->name = "นวดหน้าด้วยน้ำมันมะพร้าว";
-        $coupon->price = 900;
-        $coupon->time = 45; 
+        $coupon->name = "ดัดผมดิจิตอล";
+        $coupon->price = 3500;
+        $coupon->time = 140; 
         $coupon->save();
 
+        // type id 1 : ซาลอน
+        // service id 5 : ยืดผม 
+        // coupon id 8
         $coupon = new Coupon();
         $coupon->type_id = 1;
-        $coupon->service_id = 1;
-        $coupon->name = "ย้อมผม+กัดสีผม 1 รอบ";
-        $coupon->price = 500;
-        $coupon->time = 100; 
+        $coupon->service_id = 5;
+        $coupon->name = "ยืดวอลลุ่ม";
+        $coupon->price = 4000;
+        $coupon->time = 200; 
         $coupon->save();
 
+        // type id 2 : นวด
+        // service id 6 : นวดไทย
+        // coupon id 9
         $coupon = new Coupon();
-        $coupon->type_id = 1;
-        $coupon->service_id = 1;
-        $coupon->name = "ย้อมผม+กัดสีผม 1 รอบ";
+        $coupon->type_id = 2;
+        $coupon->service_id = 6;
+        $coupon->name = "นวดแผนไทย";
         $coupon->price = 500;
-        $coupon->time = 100; 
+        $coupon->time = 90; 
         $coupon->save();
 
+        // coupon id 10
         $coupon = new Coupon();
-        $coupon->type_id = 1;
-        $coupon->service_id = 1;
-        $coupon->name = "ย้อมผม+กัดสีผม 1 รอบ";
-        $coupon->price = 500;
-        $coupon->time = 100; 
+        $coupon->type_id = 2;
+        $coupon->service_id = 6;
+        $coupon->name = "นวดฝ่าเท้าด้วยน้ำมันมวย";
+        $coupon->price = 100;
+        $coupon->time = 30; 
         $coupon->save();
+
+        // type id 2 : นวด
+        // service id 7 : นวดอโรม่า
+        // coupon id 11
+        $coupon = new Coupon();
+        $coupon->type_id = 2;
+        $coupon->service_id = 7;
+        $coupon->name = "นวดน้ำมันอโรม่า";
+        $coupon->price = 1000;
+        $coupon->time = 90; 
+        $coupon->save();
+
+        // type id 3 : ทำเล็บ
+        // service id 8 : ทาเล็บเจล
+        // coupon id 12
+        $coupon = new Coupon();
+        $coupon->type_id = 3;
+        $coupon->service_id = 8;
+        $coupon->name = "ทาสีเจลมือ";
+        $coupon->price = 300;
+        $coupon->time = 30; 
+        $coupon->save();
+
+        // coupon id 13
+        $coupon = new Coupon();
+        $coupon->type_id = 3;
+        $coupon->service_id = 8;
+        $coupon->name = "ทาสีเจลมือ + เท้า";
+        $coupon->price = 500;
+        $coupon->time = 60; 
+        $coupon->save();
+
+        // type id 3 : ทำเล็บ
+        // service id 9 : ตัดแต่งเล็บ
+        // coupon id 14
+        $coupon = new Coupon();
+        $coupon->type_id = 3;
+        $coupon->service_id = 9;
+        $coupon->name = "ตัดแต่งเล็บ";
+        $coupon->price = 200;
+        $coupon->time = 20; 
+        $coupon->save();
+
+        // type id 3 : ทำเล็บ
+        // service id 10 : ต่อเล็บ
+        // coupon id 15
+        $coupon = new Coupon();
+        $coupon->type_id = 3;
+        $coupon->service_id = 10;
+        $coupon->name = "ต่อเล็บอะคริลิค";
+        $coupon->price = 1200;
+        $coupon->time = 60; 
+        $coupon->save();
+
+        // coupon id 16
+        $coupon = new Coupon();
+        $coupon->type_id = 3;
+        $coupon->service_id = 10;
+        $coupon->name = "ต่อเล็บ PVC";
+        $coupon->price = 1000;
+        $coupon->time = 60; 
+        $coupon->save();
+
+
+        // type id 4 : บริการเสริมความงาม
+        // service id 11 : โบท็อกซ์
+        // coupon id 17
+        
+
+        // type id 4 : บริการเสริมความงาม
+        // service id 12 : เมโสแฟต
+        // coupon id 
+        
+        // type id 4 : บริการเสริมความงาม
+        // service id 13 : ฟิลเลอร์
+        // coupon id 
+
+
+        // type id 4 : บริการเสริมความงาม
+        // service id 14 : ร้อยไหม
+        // coupon id 
+
+
+        // type id 5 : แว็กซ์
+        // service id 15 : แว็กซ์แขน ขา
+        // coupon id 
+
+
+        // type id 5 : แว็กซ์
+        // service id 16 : แว็กซ์จุดซ่อนเร้น
+        // coupon id 
+
+
+        // type id 6 : ทรีทเมนต์ (แก้คำด้วย)
+        // service id 17 : มาร์ก
+        // coupon id 
+
+
+        // type id 6 : ทรีทเมนต์
+        // service id 18 : นวดใบหน้า
+        // coupon id 
+
+
+        // type id 7 : ศัลยกรรมเฉพาะทาง
+        // service id 19 : เสริมจมูก
+        // coupon id 
+
+        // type id 7 : ศัลยกรรมเฉพาะทาง
+        // service id 20 : เสริมคาง
+        // coupon id 
+
+        // type id 7 : ศัลยกรรมเฉพาะทาง
+        // service id 21 : ทำตาสองชั้น
+        // coupon id 
+
+
+
+
+        //บึ้มมมมมมมมมมมมมมมมมมมมมมม
+        // นวดหน้า service id 4
+        // coupon id 6
+        // $coupon = new Coupon();
+        // $coupon->type_id = 2;
+        // $coupon->service_id = 4;
+        // $coupon->name = "นวดหน้าด้วยน้ำมันมะพร้าว";
+        // $coupon->price = 900;
+        // $coupon->time = 45; 
+        // $coupon->save();
+
+        // $coupon = new Coupon();
+        // $coupon->type_id = 1;
+        // $coupon->service_id = 1;
+        // $coupon->name = "ย้อมผม+กัดสีผม 1 รอบ";
+        // $coupon->price = 500;
+        // $coupon->time = 100; 
+        // $coupon->save();
+
+        // $coupon = new Coupon();
+        // $coupon->type_id = 1;
+        // $coupon->service_id = 1;
+        // $coupon->name = "ย้อมผม+กัดสีผม 1 รอบ";
+        // $coupon->price = 500;
+        // $coupon->time = 100; 
+        // $coupon->save();
+
+        // $coupon = new Coupon();
+        // $coupon->type_id = 1;
+        // $coupon->service_id = 1;
+        // $coupon->name = "ย้อมผม+กัดสีผม 1 รอบ";
+        // $coupon->price = 500;
+        // $coupon->time = 100; 
+        // $coupon->save();
 
         
 
--- database/seeders/ServiceSeeder.php
+++ database/seeders/ServiceSeeder.php
@@ -21,7 +21,7 @@ class ServiceSeeder extends Seeder
         $service = new Service();
         $service->type_id = 1;
         $service->name = "สระผม";
-        $service->description = "";
+        $service->description = "บริการสระผมแบบพรีเมียมด้วยน้ำนมวัวนำเข้าจากนิวซีแลนด์ และบำรุงเส้นผมด้วยผลิตภัณฑ์ออแกนิก";
         $service->service_image_url = "image_upload/hair-washhair.jpg";
         $service->save();
 
@@ -30,7 +30,7 @@ class ServiceSeeder extends Seeder
         $service = new Service();
         $service->type_id = 1;
         $service->name = "ตัดผม";
-        $service->description = "";
+        $service->description = "บริการตัดผมโดยช่างมือ 1 ของตำบลบ้านยาง การันตีฝีมือการตัดโดยรางวัลประกวดตัดผมประจำตำบล 4 ปีซ้อน";
         $service->service_image_url = "image_upload/hair-haircut.jpg";
         $service->save();
 
@@ -38,7 +38,7 @@ class ServiceSeeder extends Seeder
         $service = new Service();
         $service->type_id = 1;
         $service->name = "ย้อมสีผม";
-        $service->description = "";
+        $service->description = "บริการย้อมผม โดยใช้ยาย้อมผมที่อ่อนโยนต่อหนังศรีษะและเส้นผม ผมจะไม่แห้งและไม่เสียถึง 99.99%";
         $service->service_image_url = "image_upload/hair-dyehair.jpg";
         $service->save();
 
@@ -46,7 +46,7 @@ class ServiceSeeder extends Seeder
         $service = new Service();
         $service->type_id = 1;
         $service->name = "ดัดผม";
-        $service->description = "";
+        $service->description = "บริการดัดผม ใช้เครื่องลอนผมนำเข้าจากสหรัฐอเมริกา ผมจะไม่ชี้ ไม่ฟู และไม่ดีด พร้อมทั้งทำทรีทเมนต์นมแกะและสามารถกำหนดความลอนของผมได้ตามที่ต้องการ";
         $service->service_image_url = "image_upload/hair-curlhair.png";
         $service->save();
 
@@ -55,7 +55,7 @@ class ServiceSeeder extends Seeder
         $service = new Service();
         $service->type_id = 1;
         $service->name = "ยืดผม";
-        $service->description = "";
+        $service->description = "ยืดผมถาวรอยู่ได้นานถึง 1 ปี โดยใช้น้ำยาออแกนิกสูตรพิเศษจากทางร้าน ทำให้ผมของท่านตรงและเงางามได้ตลอดทั้งปี";
         $service->service_image_url = "image_upload/hair-straighten.jpg";
         $service->save();
 
@@ -64,7 +64,7 @@ class ServiceSeeder extends Seeder
         $service = new Service();
         $service->type_id = 2;
         $service->name = "บริการนวดไทย";
-        $service->description = "";
+        $service->description = "นวดแผนไทยโดยหมอนวดเฉพาะทาง จบหลักสูตรการนวดแผนไทยจากวัดโพธิ์โดยตรง สามารถนวดกดจุดช่วยบรรเทาอาการปวดตามบริเวณต่างๆได้อย่างแม่นยำ";
         $service->service_image_url = "image_upload/massage-thai.jpg";
         $service->save();
 
@@ -72,7 +72,7 @@ class ServiceSeeder extends Seeder
         $service = new Service();
         $service->type_id = 2;
         $service->name = "บริการนวดอโรม่า";
-        $service->description = "";
+        $service->description = "นวดอโรม่าด้วยน้ำมันหอมระเหย Aromatherapy จากแบรนด์ Jo Malone ช่วยบรรเทาอาการเหนื่อย และทำให้ผ่อนคลายมากขึ้น ลดอาการอ่อนล้า อ่อนเพลียของกล้ามเนื้อได้ดีเยี่ยม";
         $service->service_image_url = "image_upload/massage-aroma.jpg";
         $service->save();
 
@@ -82,7 +82,7 @@ class ServiceSeeder extends Seeder
         $service = new Service();
         $service->type_id = 3;
         $service->name = "ทาเล็บเจล";
-        $service->description = "";
+        $service->description = "บริการทาเล็บเจลสามารถเลือกสีได้ไม่จำกัดด้วยผลิตภัณฑ์คุณภาพที่ได้รับการรับรอง โดยมีสีให้เลือกมากกว่า 100 สี และสามารถเพ้นต์เล็บได้มากกว่า 500 รูปแบบ";
         $service->service_image_url = "image_upload/nail-gel.jpg";
         $service->save();
 
@@ -90,7 +90,7 @@ class ServiceSeeder extends Seeder
         $service = new Service();
         $service->type_id = 3;
         $service->name = "ตัดแต่งเล็บ";
-        $service->description = "";
+        $service->description = "บริการตัดแต่งเล็บด้วยช่างมากฝีมือและชำนาญการ ช่วยดีไซน์ตัดแต่งทั้งเล็บมือและเล็บเท้าให้เข้ากับบุคลิก เสริมสร้างภาพลักษณ์ที่ดี";
         $service->service_image_url = "image_upload/nail-cut.jpg";
         $service->save();
 
@@ -98,7 +98,7 @@ class ServiceSeeder extends Seeder
         $service = new Service();
         $service->type_id = 3;
         $service->name = "ต่อเล็บ";
-        $service->description = "";
+        $service->description = "บริการต่อเล็บทุกประเภทไม่ว่าจะเป็น PVC, สำเร็จรูป, อะคริลิคหรือแบบเจล โดยสามารถเลือกความยาวเล็บได้ตามใจชอบ ไม่จำกัดความยาวและรูปทรงของเล็บ";
         $service->service_image_url = "image_upload/nail-extension.jpg";
         $service->save();
 
@@ -108,7 +108,7 @@ class ServiceSeeder extends Seeder
         $service = new Service();
         $service->type_id = 4;
         $service->name = "โบท็อกซ์";
-        $service->description = "";
+        $service->description = "โบท็อกซ์นำเข้าจากเกาหลี เกรดพรีเมียมได้รับการรับรองมาตราฐานจากสหรัฐอเมริกา เห็นผลตั้งแต่ครั้งแรกที่ฉีด ช่วยลดรอยย่นต่างๆได้อย่างเป็นธรรมชาติ";
         $service->service_image_url = "image_upload/botox-botox.jpg";
         $service->save();
 
@@ -116,7 +116,7 @@ class ServiceSeeder extends Seeder
         $service = new Service();
         $service->type_id = 4;
         $service->name = "เมโสแฟต";
-        $service->description = "";
+        $service->description = "สลายไขมันด้วยเมโสคุณภาพตัวยานำเข้าจากประเทศเกาหลี ได้รับการรับรองจากอย. ปลอดภัย 100% เห็นผลภายใน 5 วัน";
         $service->service_image_url = "image_upload/botox-fat.jpg";
         $service->save();
 
@@ -124,7 +124,7 @@ class ServiceSeeder extends Seeder
         $service = new Service();
         $service->type_id = 4;
         $service->name = "ฟีลเลอร์";
-        $service->description = "";
+        $service->description = "บริการฉีดฟีลเลอร์ ช่วยเติมเต็มส่วนที่มีริ้วรอยให้กลับมากระชับเป็นธรรมชาติ โดยทีมแพทย์เฉพาะทางมือหนึ่งของตำบลบ้านยาง";
         $service->service_image_url = "image_upload/botox-filler.jpg";
         $service->save();
 
@@ -132,7 +132,7 @@ class ServiceSeeder extends Seeder
         $service = new Service();
         $service->type_id = 4;
         $service->name = "ร้อยไหม";
-        $service->description = "";
+        $service->description = "บริการร้อยไหม โดยใช้ไหมละลายนำเข้า เกรดพรีเมียม ยกกระชับผิวที่เหี่ยวย่น ให้กลับมาเต่งตึงเหมือนสาวแรกแย้ม";
         $service->service_image_url = "image_upload/botox-lifting.jpg";
         $service->save();
 
@@ -141,7 +141,7 @@ class ServiceSeeder extends Seeder
         $service = new Service();
         $service->type_id = 5;
         $service->name = "แว็กซ์ขนแขน ขา";
-        $service->description = "";
+        $service->description = "แว็กซ์ขนแขน ขาด้วยตัวแว็กซ์เกรดพรีเมียม ไม่ระคายเคืองผิวหนัง ไม่เป็นขนคุดพร้อมทั้งประคบเย็นและทาเจลว่านหางจระเข้ให้หลังบริการเพื่อลดอาการระคายเคือง";
         $service->service_image_url = "image_upload/wax-waxing.jpg";
         $service->save();
 
@@ -149,50 +149,50 @@ class ServiceSeeder extends Seeder
         $service = new Service();
         $service->type_id = 5;
         $service->name = "แว็กซ์จุดซ่อนเร้น";
-        $service->description = "";
+        $service->description = "แว็กซ์จุดซ่อนเร้นด้วยแว็กซ์น้ำผึ้งเดือน 5 แท้จากป่าอะเมซอน ช่วยลดการอักเสบ ไม่ระคายเคือง อ่อนโยนต่อจุดซ่อนเร้น พร้อมทั้งบริการประคบเย็นและทาว่านหางจระเข้ฟรี";
         $service->service_image_url = "image_upload/wax-bikiniwax.jpg";
         $service->save();
 
         // Treatment type id 6
         // Service id 16
         $service = new Service();
-        $service->type_id = 5;
+        $service->type_id = 6;
         $service->name = "บริการมาร์ก";
-        $service->description = "";
+        $service->description = "บริการมาร์กหน้าด้วยโคลนภูเขาไฟจากตำบลบ้านยางคุณภาพเทียบเท่ากับโคลนจากเทือกเขาไฟเกาหลี ช่วยฟื้นฟูผิวหนังให้สม่ำเสมอและกระจ่างใส ทำให้ผิวดูอ่อนเยาว์เหมือนเด็กแรกเกิด";
         $service->service_image_url = "image_upload/treatment-mask.jpg";
         $service->save();
 
         // Service id 17
         $service = new Service();
-        $service->type_id = 5;
+        $service->type_id = 6;
         $service->name = "บริการนวดใบหน้า";
-        $service->description = "";
+        $service->description = "บริการนวดใบหน้าด้วยหินกัวซาจากแบรนด์ดัง ทำให้หน้าเรียว ยกกระชับเต่งตึง ทำให้ระบบหมุนเวียนเลือดดีขึ้นและทำให้ลูกค้ารู้สึกผ่อนคลาย";
         $service->service_image_url = "image_upload/treatment-massageface.jpg";
         $service->save();
 
-        // Treatment type id 7
+        // ศัลยกรรม type id 7
         // Service id 18
         $service = new Service();
-        $service->type_id = 5;
+        $service->type_id = 7;
         $service->name = "เสริมจมูก";
-        $service->description = "";
+        $service->description = "เสริมจมูกโดยศัลยแพทย์ชั้นนำมือ 1 ของตำบล การันตีการเสริมจมูกกว่า 100 แท่ง คุณหมอเหลาซิลิโคนเองทุกเคสเพื่อให้เข้ากับรูปหน้าแต่ละคน เนียนเหมือนธรรมชาติ";
         $service->service_image_url = "image_upload/surgery-nose.jpg";
         $service->save();
 
         // Service id 19
         $service = new Service();
-        $service->type_id = 5;
+        $service->type_id = 7;
         $service->name = "เสริมคาง";
-        $service->description = "";
+        $service->description = "เสริมคางปรับแก้โหวงเฮ้งให้เข้ากับรูปหน้าและช่วยเสริมดวง โดยคุณหมอจะวิเคราะห์หาทรงคางที่ดีที่สุดที่เสริมโหวงเฮ้งใบหน้า ดูดีพร้อมกับดวงดีหาได้ที่นี่เท่านั้น";
         $service->service_image_url = "image_upload/surgery-chin.jpg";
         $service->save();
 
 
         // Service id 20
         $service = new Service();
-        $service->type_id = 5;
+        $service->type_id = 7;
         $service->name = "ทำตาสองชั้น";
-        $service->description = "";
+        $service->description = "ทำตาสองชั้น ช่วยเพิ่มความมั่นใจให้กับลูกค้าทุกท่าน ไม่ว่าจะเป็นสองชั้นธรรมดาหรือตาทรง Foxy eyes ก็สามารถทำได้เนียนอย่างเป็นธรรมชาติ ปรับเปลี่ยนให้ดูดียิ่งขึ้นเหมือนแม่ให้มา";
         $service->service_image_url = "image_upload/surgery-chin.jpg";
         $service->save();
     }
