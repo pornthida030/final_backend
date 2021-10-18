025e37cb22cd908a599a50e9f2a008ebfd751374
theyokky <Yanakorn.j@ku.th>
2021-10-18 19:17:38 +0700

    Update UserCouponSeeder.php

--- database/seeders/UserCouponSeeder.php
+++ database/seeders/UserCouponSeeder.php
@@ -21,7 +21,7 @@ class UserCouponSeeder extends Seeder
         $user_coupon->user_id = 3;
         $user_coupon->employee_id = 8;
         $user_coupon->coupon_id = 6;
-        $user_coupon->coupon_status= "used";
+        $user_coupon->coupon_status= "unuse";
         $user_coupon->reviewed = false;
         $user_coupon->save();
 
@@ -43,7 +43,7 @@ class UserCouponSeeder extends Seeder
         $user_coupon->user_id = 3;
         $user_coupon->employee_id = 9;
         $user_coupon->coupon_id = 6;
-        $user_coupon->coupon_status= "used";
+        $user_coupon->coupon_status= "unuse";
         $user_coupon->reviewed = false;
         $user_coupon->save();
 
@@ -66,7 +66,7 @@ class UserCouponSeeder extends Seeder
         $user_coupon->employee_id = 11;
         $user_coupon->user_id = 4;
         $user_coupon->coupon_id = 10;
-        $user_coupon->coupon_status= "used";
+        $user_coupon->coupon_status= "unuse";
         $user_coupon->reviewed = false;
         $user_coupon->save();
 
@@ -101,7 +101,7 @@ class UserCouponSeeder extends Seeder
         $user_coupon->employee_id = 12;
         $user_coupon->user_id = 6;
         $user_coupon->coupon_id = 9;
-        $user_coupon->coupon_status= "used";
+        $user_coupon->coupon_status= "unuse";
         $user_coupon->reviewed = false;
         $user_coupon->save();
 
@@ -111,7 +111,7 @@ class UserCouponSeeder extends Seeder
         $user_coupon->employee_id = 10;
         $user_coupon->user_id = 6;
         $user_coupon->coupon_id = 5;
-        $user_coupon->coupon_status= "used";
+        $user_coupon->coupon_status= "unuse";
         $user_coupon->reviewed = false;
         $user_coupon->save();
 
@@ -133,7 +133,7 @@ class UserCouponSeeder extends Seeder
         $user_coupon->employee_id = 8;
         $user_coupon->user_id = 7;
         $user_coupon->coupon_id = 8;
-        $user_coupon->coupon_status= "used";
+        $user_coupon->coupon_status= "unuse";
         $user_coupon->reviewed = false;
         $user_coupon->save();
     }
