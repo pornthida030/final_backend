08becb02f46079e20fc4caebd5f5ec84d76aeaa0
tataPongpol <pongpol.ro@ku.th>
2021-10-18 15:12:00 +0700

    update data in UserCouponSeeder

--- database/seeders/UserCouponSeeder.php
+++ database/seeders/UserCouponSeeder.php
@@ -14,59 +14,125 @@ class UserCouponSeeder extends Seeder
      */
     public function run()
     {
-        // user id 1
-        // coupon id 1
+        // user1
         $user_coupon = new UserCoupon();
-        $user_coupon->service_id = 1;
+        $user_coupon->service_id = 3;
 
-        $user_coupon->user_id = 2;
-        $user_coupon->employee_id = 3;
-        $user_coupon->coupon_id = 1;
-        $user_coupon->coupon_status= "used";
-        $user_coupon->reviewed = true;
-        $user_coupon->save();
-
-        // user id 1
-        // coupon id 2
-        $user_coupon = new UserCoupon();
-        $user_coupon->service_id = 1;
-
-        $user_coupon->user_id = 2;
-        $user_coupon->employee_id = 3;
-        $user_coupon->coupon_id = 2;
+        $user_coupon->user_id = 3;
+        $user_coupon->employee_id = 8;
+        $user_coupon->coupon_id = 6;
         $user_coupon->coupon_status= "used";
         $user_coupon->reviewed = false;
         $user_coupon->save();
 
-        // user id 1
-        // coupon id 3
-        $user_coupon = new UserCoupon();
-        $user_coupon->service_id = 1;
-        $user_coupon->employee_id = 3;
-        $user_coupon->user_id = 2;
-        $user_coupon->coupon_id = 2;
-        $user_coupon->coupon_status= "unuse";
-        $user_coupon->reviewed = true;
-        $user_coupon->save();
 
-        // user id 2
-        // coupon id 4
         $user_coupon = new UserCoupon();
-        $user_coupon->service_id = 4;
-        $user_coupon->employee_id = 3;
-        $user_coupon->user_id = 2;
-        $user_coupon->coupon_id = 2;
-        $user_coupon->coupon_status= "unuse";
+        $user_coupon->service_id = 3;
+
+        $user_coupon->user_id = 3;
+        $user_coupon->employee_id = 8;
+        $user_coupon->coupon_id = 5;
+        $user_coupon->coupon_status= "used";
         $user_coupon->reviewed = false;
         $user_coupon->save();
 
-        // user id 2
-        // coupon id 5
+
+        $user_coupon = new UserCoupon();
+        $user_coupon->service_id = 3;
+
+        $user_coupon->user_id = 3;
+        $user_coupon->employee_id = 9;
+        $user_coupon->coupon_id = 6;
+        $user_coupon->coupon_status= "used";
+        $user_coupon->reviewed = false;
+        $user_coupon->save();
+
+
+        // user2
+        $user_coupon = new UserCoupon();
+        $user_coupon->service_id = 6;
+
+        $user_coupon->employee_id = 11;
+        $user_coupon->user_id = 4;
+        $user_coupon->coupon_id = 9;
+        $user_coupon->coupon_status= "used";
+        $user_coupon->reviewed = false;
+        $user_coupon->save();
+
+
+        $user_coupon = new UserCoupon();
+        $user_coupon->service_id = 6;
+
+        $user_coupon->employee_id = 11;
+        $user_coupon->user_id = 4;
+        $user_coupon->coupon_id = 10;
+        $user_coupon->coupon_status= "used";
+        $user_coupon->reviewed = false;
+        $user_coupon->save();
+
+
+        $user_coupon = new UserCoupon();
+        $user_coupon->service_id = 6;
+
+        $user_coupon->employee_id = 12;
+        $user_coupon->user_id = 4;
+        $user_coupon->coupon_id = 10;
+        $user_coupon->coupon_status= "used";
+        $user_coupon->reviewed = false;
+        $user_coupon->save();
+
+
+        // user3
+        $user_coupon = new UserCoupon();
+        $user_coupon->service_id = 6;
+
+        $user_coupon->employee_id = 11;
+        $user_coupon->user_id = 5;
+        $user_coupon->coupon_id = 9;
+        $user_coupon->coupon_status= "used";
+        $user_coupon->reviewed = false;
+        $user_coupon->save();
+
+
+        // user4
+        $user_coupon = new UserCoupon();
+        $user_coupon->service_id = 6;
+
+        $user_coupon->employee_id = 12;
+        $user_coupon->user_id = 6;
+        $user_coupon->coupon_id = 9;
+        $user_coupon->coupon_status= "used";
+        $user_coupon->reviewed = false;
+        $user_coupon->save();
+
+        $user_coupon = new UserCoupon();
+        $user_coupon->service_id = 3;
+
+        $user_coupon->employee_id = 10;
+        $user_coupon->user_id = 6;
+        $user_coupon->coupon_id = 5;
+        $user_coupon->coupon_status= "used";
+        $user_coupon->reviewed = false;
+        $user_coupon->save();
+
+        $user_coupon = new UserCoupon();
+        $user_coupon->service_id = 2;
+
+        $user_coupon->employee_id = 9;
+        $user_coupon->user_id = 6;
+        $user_coupon->coupon_id = 3;
+        $user_coupon->coupon_status= "used";
+        $user_coupon->reviewed = false;
+        $user_coupon->save();
+
+
+        // user5
         $user_coupon = new UserCoupon();
         $user_coupon->service_id = 5;
-        $user_coupon->employee_id = 3;
-        $user_coupon->user_id = 2;
-        $user_coupon->coupon_id = 3;
+
+        $user_coupon->employee_id = 8;
+        $user_coupon->user_id = 7;
+        $user_coupon->coupon_id = 8;
         $user_coupon->coupon_status= "used";
         $user_coupon->reviewed = false;
         $user_coupon->save();
