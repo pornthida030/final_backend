4ba4daa74509950ba599bfdd97cb4eb2f91d4048
Pond261271 <phantouch.s@ku.th>
2021-11-07 14:38:52 +0700

    change index js

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const express = require('express')
+const cors = require('cor')
 const app = express()
+app.use(cors())
 
 app.get('/api', (req, res) => {
   res.status(200).json({api: 'version 1'})
 })
 
-app.listen(3000, () => console.log('server started'))
+app.get('/',(req,res)=>{
+  res.send("Hello world")
+})
+
+app.get('/greeting',(req,res)=>{
+  res.json({greeting:'Hello'})
+})
+
+
+
+app.listen(process.env.PORT ||3000, () => console.log('server started'))
